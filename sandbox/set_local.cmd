f929e7ef0c3f7175745fe4aee2021e43aa367def
if12 <paul_ramsden@arburg.com>
2023-04-21 12:36:27 +0200

    first commit

---
+++ sandbox/set_local.cmd
@@ -0,0 +1,11 @@
+echo off
+rem setup environment for local terminal
+rem tycho >= 3.x needs j17
+
+##set java_home=C:\Program Files\Eclipse Adoptium\jdk-11.0.18.10-hotspot
+set java_home=C:\Program Files\Eclipse Adoptium\jdk-17.0.6.10-hotspot
+
+set path=%java_home%\bin;%path%
+
+
+java -version
