0f4ae0f5c2c81ea0f8a937b8c1bf4e9276be0819
if12 <paul_ramsden@arburg.com>
2023-04-25 09:18:44 +0200

    Merge branch 'main' of https://github.com/pramsden/tycho-stackoverflow.git into main

--- sandbox/set_local.cmd
+++ sandbox/set_local.cmd
@@ -2,7 +2,7 @@ echo off
 rem setup environment for local terminal
 rem tycho >= 3.x needs j17
 
-##set java_home=C:\Program Files\Eclipse Adoptium\jdk-11.0.18.10-hotspot
+rem set java_home=C:\Program Files\Eclipse Adoptium\jdk-11.0.18.10-hotspot
 set java_home=C:\Program Files\Eclipse Adoptium\jdk-17.0.6.10-hotspot
 
 set path=%java_home%\bin;%path%
